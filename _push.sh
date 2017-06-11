5ce10cabe5ba6044470feb267249e8fc18868776
Ben Best <ben@ecoquants.com>
2017-06-11 06:52:14 -0700

    Travis deply -> _push.sh

---
+++ _push.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export GH_USER='test-new-org'
+export GH_REPO='test-new-org.github.io'
+export GH_BRANCH='master'
+
+# setup git
+git config --global user.email "ben@ecoquants.com"
+git config --global user.name "Ben Best"
+git config --global push.default matching
+
+# git commit
+git add --all
+git commit -am "Travis render & push [skip ci]: $TRAVIS_BUILD_NUMBER"
+
+# git push
+git push https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git ${GH_BRANCH}
